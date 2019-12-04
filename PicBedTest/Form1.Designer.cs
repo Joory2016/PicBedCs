110215a6b13a5ca72441dbc88b8cf927ec3eb806
睿 赵 <joory_2015@outlook.com>
2019-12-04 22:13:59 +0800

    add some buttons

--- PicBedTest/Form1.Designer.cs
+++ PicBedTest/Form1.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.lbl_gitAdd = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_copy = new System.Windows.Forms.Button();
+            this.btn_push = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.textBox1.Location = new System.Drawing.Point(169, 37);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(329, 26);
@@ -43,22 +48,64 @@
             // lbl_gitAdd
             // 
             this.lbl_gitAdd.AutoSize = true;
-            this.lbl_gitAdd.Font = new System.Drawing.Font("Microsoft YaHei", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_gitAdd.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lbl_gitAdd.Location = new System.Drawing.Point(66, 38);
             this.lbl_gitAdd.Name = "lbl_gitAdd";
-            this.lbl_gitAdd.Size = new System.Drawing.Size(87, 20);
+            this.lbl_gitAdd.Size = new System.Drawing.Size(88, 20);
             this.lbl_gitAdd.TabIndex = 1;
-            this.lbl_gitAdd.Text = "git Address:";
+            this.lbl_gitAdd.Text = "Git Address:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(70, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 206);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(398, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_copy
+            // 
+            this.btn_copy.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_copy.Location = new System.Drawing.Point(398, 268);
+            this.btn_copy.Name = "btn_copy";
+            this.btn_copy.Size = new System.Drawing.Size(105, 30);
+            this.btn_copy.TabIndex = 4;
+            this.btn_copy.Text = "Copy Add";
+            this.btn_copy.UseVisualStyleBackColor = true;
+            // 
+            // btn_push
+            // 
+            this.btn_push.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_push.Location = new System.Drawing.Point(398, 221);
+            this.btn_push.Name = "btn_push";
+            this.btn_push.Size = new System.Drawing.Size(105, 30);
+            this.btn_push.TabIndex = 4;
+            this.btn_push.Text = "Push";
+            this.btn_push.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 388);
+            this.Controls.Add(this.btn_push);
+            this.Controls.Add(this.btn_copy);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbl_gitAdd);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +115,10 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label lbl_gitAdd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_copy;
+        private System.Windows.Forms.Button btn_push;
     }
 }
 
